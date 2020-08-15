73a9c198b16846bc8a9c8605ce2b19d162fb5328
namtndus <rnrmf0721@gmail.com>
2020-08-15 22:43:24 +0900

    make change a clock

--- js-momontom/clock.js
+++ js-momontom/clock.js
@@ -5,9 +5,10 @@ function getTime() {
   const date = new Date();
   const hour = date.getHours();
   const min = date.getMinutes();
+  const sec = date.getSeconds();
   clock.innerText = `${hour < 10 ? `0${hour}` : `${hour}`}:${
     min < 10 ? `0${min}` : `${min}`
-  }`;
+  }:${sec < 10 ? `0${sec}` : `${sec}`}`;
 }
 
 function init() {
